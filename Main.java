7e88b39e6433a53a9c3643e29273fb42841ed623
Ajdamstra <149728898+Ajdamstra@users.noreply.github.com>
2025-02-18 15:16:10 -0500

    Second commit confirmation
    This updated file shows an updates print statement for the updates commit.

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.print("This is a RepositoryManagementExercise test");
 
+		System.out.print("This string of text should show after one update push");
 	}
 
 }
